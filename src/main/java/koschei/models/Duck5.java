1d7b8e7e4ff1aa33f72f5eacc27ae5cb37c3b202
Alyona275 <alena.kim.220795@gmail.com>
2021-02-13 22:44:45 +0900

    2.1.4 commit

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
-    @Autowired
     private Egg6 egg;
+
+    @Autowired
+    public void setEgg(Egg6 egg){
+        this.egg = egg;
+    }
 
     @Override
     public String toString() {
